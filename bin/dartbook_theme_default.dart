af7d353afd47f781442efd9488de57d79cac17c8
lindeer <cljs118@126.com>
2023-12-04 17:15:33 +0800

    import resource

--- bin/dartbook_theme_default.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dartbook_theme_default/dartbook_theme_default.dart' as dartbook_theme_default;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dartbook_theme_default.calculate()}!');
-}
