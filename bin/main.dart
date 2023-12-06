0da98f1f0f1c3d900294798ce226b4e417e23f45
lindeer <cljs118@126.com>
2023-12-06 09:20:32 +0800

    output package root path

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,13 @@
+import 'dart:isolate' show Isolate;
+import 'dart:io' show File;
 
-void main(List<String> args) {
+Future<String> _resolvePackageLocation(String path) async {
+  final uri = Uri.parse('package:dartbook_theme_default$path');
+  final pkgUri = await Isolate.resolvePackageUri(uri);
+  return pkgUri?.toFilePath() ?? '';
+}
+
+void main() async {
+  final path = await _resolvePackageLocation('/');
+  print(path);
 }
